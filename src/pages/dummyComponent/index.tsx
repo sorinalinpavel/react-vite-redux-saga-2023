66ed879775aab17d75060a1ebf082ded85e5380f
Sorin-Alin Pavel <sorinalinpavel@gmail.com>
2023-02-06 23:59:12 +0200

    added missing files

---
+++ src/pages/dummyComponent/index.tsx
@@ -0,0 +1,57 @@
+import { Link } from "react-router-dom";
+
+import logo from "assets/img/logo.svg";
+
+const DummyComponent = () => {
+  return (
+    <>
+      <header className="App-header">
+        <Link to={`posts`}>GO TO POSTS</Link>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <span>
+          <span>Learn </span>
+          <a
+            className="App-link"
+            href="https://reactjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux-toolkit.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux Toolkit
+          </a>
+          ,<span> and </span>
+          <a
+            className="App-link"
+            href="https://react-redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React Redux
+          </a>
+        </span>
+      </header>
+    </>
+  );
+};
+
+export default DummyComponent;
